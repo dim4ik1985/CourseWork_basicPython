049fbc8f3836cea97bc7c5b57ea00a934731c202
dim4ik85 <dim4ik1985@yandex.ru>
2022-05-18 11:10:22 +0300

    edit files

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from pprint import pprint
 
 if __name__ == '__main__':
     name_user = input('ID VK user: ')       # Запрос ID пользователя
-    # name_user = 'dim4ik0985'
     token_yandex = getpass('Токен Яндекс: ')
     try:
         num_photos = int(input('Количество фото (по умолчанию 5): '))
     except ValueError:
         num_photos = 5
-    # get_to_copy(name_user, token_yandex, num_photos)  # Вызов функции
+    get_to_copy(name_user, token_yandex, num_photos)  # Вызов функции
